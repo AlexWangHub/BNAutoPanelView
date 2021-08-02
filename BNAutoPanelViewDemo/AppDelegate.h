b2aea1d634b5b4184450f9b0e5acabe9f62adef7
binbinwang <307649087@qq.com>
2021-08-02 16:10:42 +0800

    Initial Commit

---
+++ BNAutoPanelViewDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BNAutoPanelViewDemo
+//
+//  Created by binbinwang on 2021/8/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
